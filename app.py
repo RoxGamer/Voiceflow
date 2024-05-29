f3ade3a776d02104a388165491b84b780565ddf5
Rox <67151410+RoxGamer@users.noreply.github.com>
2024-05-29 18:59:34 +0530

    Update app.py

--- app.py
+++ app.py
@@ -141,5 +141,5 @@ def get_suggestion():
 
     return jsonify({"suggestion": suggestion})  # Send as JSON response
 
-if __name__ == "__main__":
-    app.run(debug=False,host="0.0.0.0")
+# if __name__ == "__main__":
+#     app.run(debug=False,host="0.0.0.0")
